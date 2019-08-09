0c73923faf44558ae63ea050059a49aff2a6a291
Yuri Poquini <gabril.yuri.1906@gmail.com>
2019-08-08 22:16:26 -0300

    Renderizando lista de usuarios

--- frontend/src/pages/main.js
+++ frontend/src/pages/main.js
@@ -1,88 +1,57 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './main.css'
+
+import api from '../services/api'
 
 import logo from '../assets/logo.svg'
 import dislike from '../assets/dislike.svg'
 import like from '../assets/like.svg'
 
 export default function Main({ match }) {
+    const [users, setUsers] = useState([]);
+
+    useEffect(() => {
+        async function loadUsers() {
+            const response = await api.get('/devs', {
+                headers: {
+                    user: match.params.id,
+                }
+            });
+
+            setUsers(response.data)
+
+        }
+        loadUsers();
+    }, [match.params.id]);
+
     return (
 
         <div className="main-container">
             <img src={logo} alt="TinDev" />
 
             <ul>
-                <li>
-                    <img src="https://avatars1.githubusercontent.com/u/41344686?v=4" alt=" " />
-                    <footer>
-                        <strong>My Name</strong>
-                        <p>Hello my name is yuri and i love eat ice cream</p>
-                    </footer>
+                {users.map(user => (
+                    <li key= {user._id}>
+                        <img src={user.avatar} alt=" " />
+                        <footer>
+                            <strong>{user.name}</strong>
+                            <p>{user.bio}</p>
+                        </footer>
 
-                    <div className="buttons">
-                        <button type="button">
-                            <img src={dislike} alt="Dislike" />
-                        </button>
+                        <div className="buttons">
+                            <button type="button">
+                                <img src={dislike} alt="Dislike" />
+                            </button>
 
-                        <button type="button">
-                            <img src={like} alt="Like" />
-                        </button>
-                    </div>
-                </li>
+                            <button type="button">
+                                <img src={like} alt="Like" />
+                            </button>
+                        </div>
+                    </li>
+                ))}
 
-                <li>
-                    <img src="https://avatars0.githubusercontent.com/u/26507200?v=4" alt=" " />
-                    <footer>
-                        <strong>My Name</strong>
-                        <p>Hello my name is yuri and i love eat ice cream</p>
-                    </footer>
 
-                    <div className="buttons">
-                        <button type="button">
-                            <img src={dislike} alt="Dislike" />
-                        </button>
 
-                        <button type="button">
-                            <img src={like} alt="Like" />
-                        </button>
-                    </div>
-                </li>
-
-                <li>
-                    <img src="https://avatars1.githubusercontent.com/u/33640684?v=4" alt=" " />
-                    <footer>
-                        <strong>My Name</strong>
-                        <p>Hello my name is yuri and i love eat ice cream</p>
-                    </footer>
-
-                    <div className="buttons">
-                        <button type="button">
-                            <img src={dislike} alt="Dislike" />
-                        </button>
-
-                        <button type="button">
-                            <img src={like} alt="Like" />
-                        </button>
-                    </div>
-                </li>
-
-                <li>
-                    <img src="https://avatars2.githubusercontent.com/u/487669?v=4" alt=" " />
-                    <footer>
-                        <strong>My Name</strong>
-                        <p>Hello my name is yuri and i love eat ice cream</p>
-                    </footer>
-
-                    <div className="buttons">
-                        <button type="button">
-                            <img src={dislike} alt="Dislike" />
-                        </button>
-
-                        <button type="button">
-                            <img src={like} alt="Like" />
-                        </button>
-                    </div>
-                </li>
             </ul>
         </div>
 
